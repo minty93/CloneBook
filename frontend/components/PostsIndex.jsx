65b900c4ec555657277f044e6cda46ec70b29b05
minty93 <aajain93@gmail.com>
2016-02-24 15:30:36 -0500

    Change Ads

--- frontend/components/PostsIndex.jsx
+++ frontend/components/PostsIndex.jsx
@@ -54,9 +54,9 @@ var PostsIndex = React.createClass({
         </ul>
       </div>
       <div className="ads group">
+      <a href="https://www.aarushijain.me/"><img src="http://s12.postimg.org/xz0xjn0v1/Screen_Shot_2016_02_24_at_3_27_44_PM.png"/></a>
+        <a href="http://www.github.com/minty93"><img src="http://cdn.inquisitr.com/wp-content/uploads/2015/05/Github.jpg"/></a>
         <a href="http://www.appacademy.io/"><img src="http://s30.postimg.org/wvcuhclo1/1441118500_app_academy.png"/></a>
-        <a href="http://www.github.com/"><img src="http://cdn.inquisitr.com/wp-content/uploads/2015/05/Github.jpg"/></a>
-        <a href="https://slack.com/"><img src="http://cdn.churchm.ag/wp-content/uploads/2015/03/Slack-Logoness-750x431.png"/></a>
       </div>
     </div>
     );
